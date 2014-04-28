88c8e348a836f4533749a2f0a338a983411a46f1
Ruslan Kesheshyan <ruslan.kesheshyan@gmail.com>
2014-04-28 03:08:28 +0300

    [DEV] Added: Animations, frames, controls etc.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('spriteApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $rootScope) {
     $scope.animationStructure = {
       imageSource: null,
       animations: []
@@ -14,18 +14,90 @@ angular.module('spriteApp')
       },
       background: 'url(images/bg-greed.png)',
       currentAnimation: null,
+      currentFrame: null,
       displayAnimationPanel: true
     };
 
+    /**
+     * Triggers a global change event for scope.
+     * It's needed for deep structures which can't be observed in ordinal way,
+     * because they have circular links.
+     */
+    $scope.change = function () {
+      $rootScope.$emit('controls:changeProperty');
+    };
+
+    /**
+     * Adds new animation
+     */
     $scope.addAnimation = function () {
       var nextAnimNum = ($scope.animationStructure.animations.length + 1);
       $scope.animationStructure.animations.push({
         id: nextAnimNum,
         name: 'Animation set ' + nextAnimNum,
-        width: 16,
-        height: 16,
+        width: 64,
+        height: 64,
         frames: []
       });
-      $scope.editorOptions.currentAnimation = nextAnimNum;
+      $scope.editorOptions.currentAnimation = _.last($scope.animationStructure.animations);
     };
+
+    /**
+     * Removes animation
+     * @param animation
+     */
+    $scope.removeAnimation = function (animation) {
+      if (confirm('Are you sure you want to delete this animation?')) {
+        _.remove($scope.animationStructure.animations, animation);
+        animation.frames.forEach(function (frame) {
+          frame.remove();
+        });
+      }
+    };
+
+    /**
+     * Removes frame in animation
+     * @param animation
+     * @param frame
+     */
+    $scope.removeAnimationFrame = function (animation, frame) {
+      if (confirm('Are you sure you want to delete this frame?')) {
+        _.remove(animation.frames, frame);
+        frame.remove();
+      }
+    };
+
+    /**
+     * Set current animation and sets first framse as current frame
+     * @param animation
+     */
+    $scope.setCurrentAnimation = function (animation) {
+      $scope.editorOptions.currentAnimation = animation;
+      $scope.editorOptions.currentFrame = animation.frames[0] || null;
+    };
+
+    /**
+     * Modifies the structure to the final view and triggers popup,
+     * where user can copy the structure by Ctrl+C
+     */
+    $scope.copyToClipboard = function () {
+      var resultStructure = {
+        animations: [],
+        image: $scope.animationStructure.imageSource
+      };
+
+      $scope.animationStructure.animations.forEach(function (animation) {
+        var animModified = _.pick(animation, 'name', 'width', 'height');
+        // Pick only appropriate fields
+        animModified.frames = animation.frames.map(function (frame) {
+          return _.pick(frame, 'width', 'height', 'left', 'top');
+        });
+
+        resultStructure.animations.push(animModified);
+      });
+
+      window.prompt("Copy to clipboard: Ctrl+C, Enter", JSON.stringify(resultStructure));
+    };
+
+
   });
--- app/scripts/directives/sprite-editor.js
+++ app/scripts/directives/sprite-editor.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('spriteApp')
-  .directive('spriteEditor', function (fabric) {
+  .directive('spriteEditor', function (fabric, $rootScope) {
     return {
       template: '<div class="sprite-editor" ><canvas id="main-canvas" style="background: {{ editorOptions.background }}"></canvas></div>',
       restrict: 'E',
@@ -43,15 +43,47 @@ angular.module('spriteApp')
           // Init state
           state = {
             isMouseDown: false,
-            isMouseMove: false,
-            isObjectScaling: false,
-            isObjectMoving: false,
             start: {
               x: 0,
               y: 0
-            },
-            activeObject: null
+            }
           };
+        };
+
+        /**
+         * Init all appropriate watcher for the current scope
+         */
+        var initWatchers = function () {
+          // Watcher for animation collection
+          scope.$watchCollection('structure.animations', function () {
+            canvas.renderAll();
+          });
+
+          // Watcher for current animation
+          scope.$watch('editorOptions.currentAnimation', function (currentAnimation) {
+            if (!currentAnimation) {
+              return;
+            }
+            // Hide all frames from other animation sets
+            canvas.getObjects().forEach(function (frame) {
+              frame.visible = (frame.animationSet === currentAnimation);
+            });
+            canvas.renderAll();
+          });
+
+          // Watcher for current frame
+          scope.$watch('editorOptions.currentFrame', function (frame) {
+            if (!frame) {
+              return;
+            }
+            canvas.setActiveObject(frame);
+            canvas.renderAll();
+          });
+
+          // Global change event
+          $rootScope.$on('controls:changeProperty', function () {
+            canvas.renderAll();
+          });
         };
 
         /**
@@ -78,7 +110,33 @@ angular.module('spriteApp')
           setSelectableForAllObjects(true);
         };
 
-
+        /**
+         * Creates and return new fabric.Rect
+         * @param left
+         * @param top
+         * @param width
+         * @param height
+         * @returns {fabric.Rect}
+         */
+        var createRectangle = function (left, top, width, height) {
+          return new fabric.Rect({
+            left: left,
+            top: top,
+            width: width,
+            height: height,
+            selectable: true,
+            fill: 'rgba(0, 255, 255, 0.2)',
+            hasBorders: true,
+            hasControls: true,
+            stroke: '#000000',
+            cornerColor: '#000000',
+            opacity: 0.9,
+            hasRotatingPoint: false,
+            transparentCorners: false,
+            cornerSize: 6,
+            animationSet: scope.editorOptions.currentAnimation
+          });
+        };
         /**
          * Handler of mouse down event. Starts the draw process
          * @param event
@@ -109,10 +167,20 @@ angular.module('spriteApp')
          * @param event
          */
         var onMouseUp = function (event) {
-          var dimensions = {
-            w: 0,
-            h: 0
-          };
+          // Add this rectangle to structure into current animation
+          if (!scope.editorOptions.currentAnimation) {
+            alert('You should add at least one animation!');
+            return;
+          }
+          var rectangle,
+              dimensions = {
+                w: 0,
+                h: 0
+              },
+              position = {
+                left: 0,
+                top: 0
+              };
 
           if (!state.isMouseDown) {
             return;
@@ -125,55 +193,136 @@ angular.module('spriteApp')
           // Calculate dimensions
           dimensions.w = Math.abs(event.e.offsetX - state.start.x);
           dimensions.h = Math.abs(event.e.offsetY - state.start.y);
-
-          // We don't need to add rectangle with zero dimensions
-          if (!dimensions.w && !dimensions.w) {
-            return;
+          position.left = (event.e.offsetX < state.start.x) ? event.e.offsetX : state.start.x;
+          position.top = (event.e.offsetY < state.start.y) ? event.e.offsetY : state.start.y;
+          // Set animation's default dimension in case user has just clicked
+          if (dimensions.w < 2 || dimensions.w < 2) {
+            dimensions.w = scope.editorOptions.currentAnimation.width;
+            dimensions.h = scope.editorOptions.currentAnimation.height;
+            position.top -= Math.round(dimensions.h / 2);
+            position.left -= Math.round(dimensions.w / 2);
           }
 
           // Create rectangle
-          state.activeObject = new fabric.Rect({
-            left: (event.e.offsetX < state.start.x) ? event.e.offsetX : state.start.x,
-            top:  (event.e.offsetY < state.start.y) ? event.e.offsetY : state.start.y,
-            selectable: true,
-            fill: '#ff00ff',
-            hasBorders: true,
-            hasControls: true,
-            opacity: 0.3,
-            hasRotatingPoint: false,
-            transparentCorners: false,
-            cornerSize: 6,
-            width: dimensions.w,
-            height: dimensions.h
-          });
-          canvas.add(state.activeObject);
+          rectangle = createRectangle(position.left, position.top, dimensions.w, dimensions.h);
+
+          scope.editorOptions.currentAnimation.frames.push(rectangle);
+          scope.editorOptions.currentFrame = rectangle;
+          canvas.add(rectangle);
           canvas.renderAll();
+          // We don't need make apply here, cause is will be fired in the object:selected handler bellow
+          scope.$apply();
         };
 
         /**
-         * Handler of object scaling event. Ends drawing
+         * Handler for object scaling event. Ends drawing
          * @param event
          */
-        var onObjectScaling = function (options) {
-          state.isMouseDown = false;
-          unFreeze();
-        };
-
-        /**
-         * Handler of object moving event. Ends drawing
-         * @param event
-         */
-        var onObjectMoving = function (options) {
+        var onObjectScaling = function (event) {
           state.isMouseDown = false;
           unFreeze();
           // Snapping to greed
           if (scope.editorOptions.snapToGreed) {
-            options.target.set({
-              left: Math.round(options.target.left / scope.editorOptions.greedSize.width) * scope.editorOptions.greedSize.width,
-              top: Math.round(options.target.top / scope.editorOptions.greedSize.height) * scope.editorOptions.greedSize.height
+            event.target.set({
+              left: Math.round(event.target.left),
+              top: Math.round(event.target.top),
+              width: Math.round(event.target.getWidth()) || 1,
+              height: Math.round(event.target.getHeight()) || 1,
+              scaleX: 1,
+              scaleY: 1
             });
           }
+          scope.$apply();
+        };
 
+        /**
+         * Handler for object moving event. Ends drawing
+         * @param event
+         */
+        var onObjectMoving = function (event) {
+          state.isMouseDown = false;
+          unFreeze();
+          // Snapping to greed
+          if (scope.editorOptions.snapToGreed) {
+            event.target.set({
+              left: Math.round(event.target.left / scope.editorOptions.greedSize.width) * scope.editorOptions.greedSize.width,
+              top: Math.round(event.target.top / scope.editorOptions.greedSize.height) * scope.editorOptions.greedSize.height
+            });
+          }
+          scope.$apply();
+        };
+
+        /**
+         * Handler for object moving event. Ends drawing
+         * @param event
+         */
+        var onObjectSelected = function (event) {
+          if (scope.editorOptions.currentFrame === event.target) {
+            return;
+          }
+          scope.editorOptions.currentFrame = event.target;
+          scope.$apply();
+        };
+
+        /**
+         * Keyboard controls
+         * @param event
+         */
+        var onKeyDown = function (event) {
+          var activeObject = canvas.getActiveObject();
+          if (!activeObject || event.target.tagName.toLowerCase() === 'input') {
+            return;
+          }
+          console.log(event);
+          if (!event.shiftKey) {
+            switch(event.keyIdentifier) {
+              case 'Left':
+                activeObject.setLeft(activeObject.getLeft() - 1);
+                break;
+              case 'Right':
+                activeObject.setLeft(activeObject.getLeft() + 1);
+                break;
+              case 'Up':
+                activeObject.setTop(activeObject.getTop() - 1);
+                break;
+              case 'Down':
+                activeObject.setTop(activeObject.getTop() + 1);
+                break;
+              case 'U+007F': // Delete
+                canvas.remove(activeObject);
+                _.remove(scope.editorOptions.currentAnimation.frames, activeObject);
+                break;
+              case 'U+0043': // C - copy and paste at once
+                var newObject = createRectangle(activeObject.getLeft() + activeObject.getWidth(), activeObject.getTop(), activeObject.getWidth(), activeObject.getHeight());
+                canvas.add(newObject);
+                canvas.setActiveObject(newObject);
+                scope.editorOptions.currentAnimation.frames.push(newObject);
+                break;
+              default:
+                break;
+            }
+          } else {
+            switch(event.keyIdentifier) {
+              case 'Left':
+                activeObject.setWidth(activeObject.getWidth() - 1);
+                break;
+              case 'Right':
+                activeObject.setWidth(activeObject.getWidth() + 1);
+                break;
+              case 'Up':
+                activeObject.setHeight(activeObject.getHeight() - 1);
+                break;
+              case 'Down':
+                activeObject.setHeight(activeObject.getHeight() + 1);
+                break;
+              default:
+                break;
+            }
+          }
+          event.stopPropagation();
+          event.preventDefault();
+          scope.$apply();
+          canvas.renderAll();
         };
         /**
          * Add event handlers for the canvas
@@ -183,9 +332,12 @@ angular.module('spriteApp')
           canvas.on('mouse:up', onMouseUp);
           canvas.on('object:scaling', onObjectScaling);
           canvas.on('object:moving', onObjectMoving);
+          canvas.on('object:selected', onObjectSelected);
+          document.addEventListener("keydown", onKeyDown, false);
         };
 
         init();
+        initWatchers();
         addEventHandlers();
       }
     };
